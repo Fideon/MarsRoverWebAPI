ed19aeab4075e2f47d5ede11ef7b621b051a709e
Fidel Salazar <fidelsf91@gmail.com>
2020-09-30 21:08:43 -0500

    Added constraints for JSON call. Added GET logic. More changes

--- MarsRoverWebAPI/Controllers/RoverController.cs
+++ MarsRoverWebAPI/Controllers/RoverController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -16,24 +17,48 @@ namespace MarsRoverWebAPI.Controllers
     {
         // GET api/<RoverController>/1
         [HttpGet("{id}")]
-        public string Get(int id)
+        public RoverResponse Get(string id)
         {
-            return "value";
+            // Check JSON values are within constraints
+            if (id.Length > 10)
+            {
+                return new RoverResponse { Message = "Rover ID is over the allowed limit. Rover ID should be less than 10 characters.", CurrentPosition = "()" };
+            }
+
+            Rover rover = SessionRoverExtension.GetObject<Rover>(HttpContext.Session, id);
+
+            if (rover == null)
+                return new RoverResponse { Message = "Rover does not exist.", CurrentPosition = "()" };
+
+            return new RoverResponse { Message = $"Rover {rover.RoverId}'s orientation is: {rover.RoverOrientation}", CurrentPosition = $"({rover.RoverX},{rover.RoverY})" };
         }
 
         // POST api/<RoverController>
         [HttpPost]
         public RoverResponse Post([FromBody] RoverInstructions value)
         {
-            // Retreive rover. If rover does not exist, then create a new one.
+            // REGular EXpression for checking instructions are limited to R, M, and L characters.
+            Regex instructionsRegex = new Regex(@"^[RML]+$");
+
+            // Check JSON values are within constraints
+            if (value.RoverId.Length > 10 || value.MovementInstruction.Length > 100)
+            {
+                return new RoverResponse { Message = "Rover ID and/or Movement Instructions are over the allowed limit. Rover ID should be less than 10 characters. Instructions should be less than 100 characters.", CurrentPosition = "()" };
+            }
+            else if (!instructionsRegex.IsMatch(value.MovementInstruction))
+            {
+                return new RoverResponse { Message = "Movement Instructions are incorrect. Instructions can only have values of 'R', 'L', and 'M'.", CurrentPosition = "()" };
+            }
+
+            // Retreive rover from Session Storage. If rover does not exist, then create a new one.
             Rover rover = SessionRoverExtension.GetObject<Rover>(HttpContext.Session, value.RoverId) == null ? new Rover(value.RoverId, "N", 0, 0) : SessionRoverExtension.GetObject<Rover>(HttpContext.Session, value.RoverId);
 
             // Execute all instructions sent for rover.
             rover = RoverOperationsService.ExecuteInstructions(value.MovementInstruction, rover);
 
-            // Save rover, and return result.
+            // Save rover to Session Storage, and return result.
             SessionRoverExtension.SetObject(HttpContext.Session, value.RoverId, rover);
-            return new RoverResponse { Message = $"Rover {rover.RoverId} has moved. Orientation is {rover.RoverOrientation}.", CurrentPosition = $"({rover.RoverX},{rover.RoverY})" };
+            return new RoverResponse { Message = $"Rover {rover.RoverId} has moved. Orientation is: {rover.RoverOrientation}.", CurrentPosition = $"({rover.RoverX},{rover.RoverY})" };
         }
     }
 }
