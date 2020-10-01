9266c890825c8a150bffd2998bff45adb4179e65
Fidel Salazar <fidelsf91@gmail.com>
2020-09-30 21:24:07 -0500

    Removed a default comment by Microsoft

--- MarsRoverWebAPI/Controllers/RoverController.cs
+++ MarsRoverWebAPI/Controllers/RoverController.cs
@@ -6,8 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace MarsRoverWebAPI.Controllers
 {
     [Route("api/[controller]")]
